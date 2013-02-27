0b8602aaca5553772fea7483ba4c6deaff7984c1
sillybowen <bz465@nyu.edu>
2013-02-27 13:48:55 -0500

    final change2

--- hw1/src/evaluate.sh
+++ hw1/src/evaluate.sh
@@ -5,7 +5,7 @@ outFiles=(../results/hw1.3-vsm.tsv ../results/hw1.3-ql.tsv ../results/hw1.3-phra
 sed -e 's/ /%20/g' ../data/queries.tsv > queries.txt
 
 for((i=0; i<${#rankers[*]}; i++))do
-   rm -rf ${outFiles[$i]}
+#   rm -rf ${outFiles[$i]}
    while read line; do
        curl "localhost:25809/search?query=$line&ranker=${rankers[$i]}&format=text" | \java edu.nyu.cs.cs2580.Evaluator ../data/qrels.tsv  ${rankers[$i]}
    done < queries.txt
